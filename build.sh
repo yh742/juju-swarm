5151a124e13a0f828a0442db82512a99d65541a3
Sean Hsu <yh742@cornell.edu>
2019-03-12 12:47:45 -0500

    added swarm reactive script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo charm build .
-mkdir ~/charms/builds/swarm-mode
-sudo cp -r /tmp/charm-builds/swarm-mode ~/charms/builds/swarm-mode
-cd ~/charms/builds/swarm-mode
+# mkdir ~/charms/builds/swarm-mode
+# rm -rf ~/charm/builds/swarm-mode/*
+#sudo cp -r /tmp/charm-builds/swarm-mode ~/charms/builds/swarm-mode
+juju deploy /tmp/charm-builds/swarm-mode/ -n 2
 # python3 tests/10-deploy
--- reactive/swarm-mode.py
+++ reactive/swarm-mode.py
@@ -1,24 +1,45 @@
+import subprocess
+import socket
 from charms.reactive import when, when_not, set_flag
-from charmhelpers.core.hookenv import is_leader
+from charmhelpers.core.hookenv import is_leader, status_set, leader_set, leader_get, log
+
+SWARM_TOKEN = ['docker','swarm','join-token','worker','-q']
 
 @when('docker.available')
-def join_swarm():
-    if is_leader:
-        status_set('maintenance', 'swarm init')
+@when_not('swarm-mode.available')
+# activate swarm for new nodes
+def leader_swarm():
+    SWARM_PORT = '2377'
+    SWARM_INIT = ['docker','swarm','init']
+    SWARM_JOIN = ['docker','swarm','join','--token']
+    if is_leader():
+        try:
+            output = subprocess.check_output(SWARM_INIT)
+            token = subprocess.check_output(SWARM_TOKEN).decode('utf-8').strip()
+            log(str(output),'DEBUG')
+            log(str(token),'DEBUG')
+            leader_set({
+                'cluster-worker-token': token,
+                'cluster-leader-ip': socket.gethostbyname(socket.gethostname())
+            })
+            status_set('active', 'swarm leader set')
+            set_flag('swarm-mode.available')
+        except Exception as e:
+            log(str(e), 'ERROR')
+            status_set('blocked', 'swarm init error')
     else:
-        status_set('maintenance', 'swarm join')
-
-@when_not('swarm-mode.installed')
-def install_swarm_mode():
-    # Do your setup here.
-    #
-    # If your charm has other dependencies before it can install,
-    # add those as @when() clauses above., or as additional @when()
-    # decorated handlers below
-    #
-    # See the following for information about reactive charms:
-    #
-    #  * https://jujucharms.com/docs/devel/developer-getting-started
-    #  * https://github.com/juju-solutions/layer-basic#overview
-    #
-    set_flag('swarm-mode.installed')
+        token = leader_get('cluster-worker-token')
+        ip = leader_get('cluster-leader-ip')
+        cmd = SWARM_JOIN[:]
+        cmd.append(token)
+        cmd.append(ip + ':' + SWARM_PORT)
+        log(str(cmd),'DEBUG')
+        try:
+            output = subprocess.check_output(cmd)
+            log(str(output),'DEBUG')
+            status_set('active', 'swarm worker set')
+            set_flag('swarm-mode.available')
+        except Exception as e:
+            log(str(e), 'ERROR')
+            status_set('blocked', 'swarm join error')
+    
